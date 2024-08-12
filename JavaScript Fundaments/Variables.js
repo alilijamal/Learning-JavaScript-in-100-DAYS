a4a1fe51b6d51faa2c0e8fd38faecb4dc050e06f
alilijamal <ali.jamal@strathmore.edu>
2024-08-12 11:08:30 +0300

    first commit

---
+++ JavaScript Fundaments/Variables.js
@@ -0,0 +1,15 @@
+//Most of the time, a JavaScript application needs to work with information. Here are two examples:
+    //An online shop – the information might include goods being sold and a shopping cart.
+    //A chat application – the information might include users, messages, and much more.
+
+
+//The statement below creates (in other words: declares) a variable with the name “message”:
+let message;
+message = 'Hello'; // store the string 'Hello' in the variable named message
+
+
+//The string is now saved into the memory area associated with the variable. We can access it using the variable name:
+alert(message); // shows the variable content
+
+//We can also declare multiple variables in one line:
+let user = 'John', age = 25, messages = 'Hello';
