f826634550ddca03bed09e0e8f769736dc309648
alilijamal <ali.jamal@strathmore.edu>
2024-08-12 12:17:08 +0300

    Loops

---
+++ JavaScript Fundaments/LoopsWhileFor.js
@@ -0,0 +1,19 @@
+//The "WHILE" loop
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+  alert( i );
+  i++;
+}
+
+//The "do....while" loop
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while (i < 3);
+
+//The "for" loop
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+    alert(i);
+  }
+
