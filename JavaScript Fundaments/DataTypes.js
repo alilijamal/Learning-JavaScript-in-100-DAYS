9dc2fd19b80d28b5ca42aa3ff7aaa71f68de81fc
alilijamal <ali.jamal@strathmore.edu>
2024-08-12 11:17:26 +0300

    Data types and its examples

---
+++ JavaScript Fundaments/DataTypes.js
@@ -0,0 +1,16 @@
+//Number
+let n = 123;
+n = 12.345;
+
+// BIGINT: the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+//String
+//A string in JavaScript must be surrounded by quotes.
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+//Boolean (logical type)
+let nameFieldChecked = true; // yes, name field is checked
+let ageFieldChecked = false; // no, age field is not checked
